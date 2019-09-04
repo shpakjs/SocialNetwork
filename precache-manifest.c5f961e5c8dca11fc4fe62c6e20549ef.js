b07780546846cd8a631e1bce61e04279a88044b0
shpakjs <shpakjs@gmail.com>
2019-09-04 22:57:05 +0300

    Updates

---
+++ precache-manifest.c5f961e5c8dca11fc4fe62c6e20549ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4fd3ac9a8d1b8ac8d1ff9580bdf21b",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "8e831af921234cc3860a",
+    "url": "/SocialNetwork/static/css/main.ada549db.chunk.css"
+  },
+  {
+    "revision": "00a76b1dc85448a25420",
+    "url": "/SocialNetwork/static/js/2.edb8d6e7.chunk.js"
+  },
+  {
+    "revision": "8e831af921234cc3860a",
+    "url": "/SocialNetwork/static/js/main.f4fc88ab.chunk.js"
+  },
+  {
+    "revision": "636a0223770ff407f4fe",
+    "url": "/SocialNetwork/static/js/runtime~main.82579fc5.js"
+  },
+  {
+    "revision": "d9c7a975d6b9e701141d1f13d486a244",
+    "url": "/SocialNetwork/static/media/login.d9c7a975.jpg"
+  },
+  {
+    "revision": "8c636777842c1f8b146dc16b84c0b154",
+    "url": "/SocialNetwork/static/media/preloader.8c636777.gif"
+  },
+  {
+    "revision": "c725ee326a418d324aca5c5c80c2b39e",
+    "url": "/SocialNetwork/static/media/profile-bg.c725ee32.jpg"
+  },
+  {
+    "revision": "ab9cd3a5ea041f028ea62dc69630003f",
+    "url": "/SocialNetwork/static/media/puzzle.ab9cd3a5.svg"
+  },
+  {
+    "revision": "44a5c053241c898dc64dac9f2f8eb23d",
+    "url": "/SocialNetwork/static/media/user.44a5c053.png"
+  }
+]);
