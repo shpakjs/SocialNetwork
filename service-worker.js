6a764ce3fa6a923ab124147ff291211c9c576167
Iuliia Shpak <shpakjs@gmail.com>
2019-09-13 13:21:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SocialNetwork/precache-manifest.b093871ec5be9a130f6d03dab52e549f.js"
+  "/SocialNetwork/precache-manifest.747efbf857954e718bf9d5c2d28828e2.js"
 );
 
 self.addEventListener('message', (event) => {
