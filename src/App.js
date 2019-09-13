b6d382e7f2fa2c0a09fb8348da1cb3018ed28b41
Iuliia Shpak <shpakjs@gmail.com>
2019-09-13 13:00:45 +0300

    Added Lazy loading for App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import {Route, withRouter} from 'react-router-dom';
 import './App.css';
 import NavbarContainer from './components/Navbar/NavbarContainer';
-import DialogsContainer from './components/Dialogs/DialogsContainer';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
-import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import LoginContainer from './components/Login/LoginContainer';
 import { initialize } from './redux/appReducer';
 import { connect } from 'react-redux';
@@ -16,6 +10,14 @@ import Preloader from './components/common/Preloader/Preloader';
 import {Provider} from 'react-redux';
 import {HashRouter} from 'react-router-dom';
 import store from './redux/reduxStore';
+import { withSuspense } from './hoc/withSuspense';
+//lazy loading
+const DialogsContainer = React.lazy( () => import('./components/Dialogs/DialogsContainer' ));
+const News = React.lazy( () => import('./components/News/News') );
+const Music = React.lazy( () => import('./components/Music/Music') );
+const Settings = React.lazy( () => import('./components/Settings/Settings'));
+const UsersContainer = React.lazy( () => import('./components/Users/UsersContainer'));
+const ProfileContainer = React.lazy( () => import('./components/Profile/ProfileContainer'));
 
 class App extends React.Component {
   componentDidMount () {
@@ -27,17 +29,13 @@ class App extends React.Component {
       return (<div className="app-wraper">
         <NavbarContainer />
         <div className="app-wraper-content">
-            <Route path= '/profile:userId?' render ={ () => 
-              <ProfileContainer/>
-            }/>
-            <Route path= '/dialogs' render ={ () => 
-              <DialogsContainer/> 
-            } />
-            <Route path= '/users' component = { () => <UsersContainer/> } />
-            <Route path= '/news' component = {News} />
-            <Route path= '/music' component = {Music} />
-            <Route path= '/settings' component = {Settings} />
-            <Route path= '/login' component = { () => <LoginContainer/> } />
+            <Route path= '/profile:userId?' render ={withSuspense(ProfileContainer)}/>
+            <Route path= '/dialogs' render ={withSuspense(DialogsContainer)} />
+            <Route path= '/users' component = {withSuspense(UsersContainer)}/>
+            <Route path= '/news' component =  {withSuspense(News)} />
+            <Route path= '/music' component = {withSuspense(Music)} />
+            <Route path= '/settings' component = {withSuspense(Settings)} />
+            <Route path= '/login' component = {<LoginContainer/>} />
         </div>
       </div>)
     } else { return <Preloader /> }
